27f4c34bd8e4e49a3abbc422eef5c4cc6ba2edba
nmluong93 <38637627+nmluong93@users.noreply.github.com>
2018-09-23 11:47:12 +0700

    Form show data

--- form-handling/src/app/app.component.ts
+++ form-handling/src/app/app.component.ts
@@ -8,15 +8,33 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    userName: '',
+    email: '',
+    secretQuestion: '',
+    answer : '',
+    gender : ''
+  };
+  submitted = false;
+
+
   suggestUserName() {
-    const userName = 'SUperuser';
+    const suggestedUserName = 'Superuser';
+    
+    // patchValue : => just set specific value
+    this.signupForm.form.patchValue({ UserData: { userName: suggestedUserName } });
   }
 
-  // onSubmit(form : NgForm) {
-  //   console.log(form);
-  // }
   onSubmit() {
     console.log(this.signupForm);
-
+    this.user.userName = this.signupForm.value.UserData.userName;
+    this.user.email = this.signupForm.value.UserData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
   }
 }
